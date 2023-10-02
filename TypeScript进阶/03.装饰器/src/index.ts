b16a6eae259636cf8551c34cd518d4222f5c26d1
t-jie.xu3 <18330156357@163.com>
2023-10-02 18:24:38 +0800

    类装饰器完成

--- TypeScript进阶/03.装饰器/src/index.ts
+++ TypeScript进阶/03.装饰器/src/index.ts
@@ -1,7 +1,49 @@
-function test(target:new() => object) {
-    console.log('target',target)
+/**
+ * 
+ * @param target 被装饰的类
+ * @returns 
+ */
+function test(target:new(...args:any[]) => object) {
+   
 }
 @test
 class A {
+    prop1:string
+
+    constructor(public prop2:string) {}
+}
+
+// const a = new A();
+
+
+function test2(str:string) {
+    return function (target:new (...args:any[]) => object) {
+        console.log('target',target)
+    } 
+}
+@test2('这是一个类')
+class B {
+
+}
+
+type constructor = new (...args:any[]) => object
+
+function d1() {
+    console.log('d1');
+    return function(target:constructor) {
+        console.log('d1 decorator');
+    }
+}
+
+function d2() {
+    console.log('d2');
+    return function(target:constructor) {
+        console.log('d2 decorator');
+    }
+}
+
+@d1()
+@d2()
+class C {
 
 }
