03bf652b4245be2f08c512fbc9ce24bfc01dc789
t-jie.xu3 <18330156357@163.com>
2023-10-07 22:14:17 +0800

    成员装饰器

--- TypeScript进阶/03.装饰器/src/index.ts
+++ TypeScript进阶/03.装饰器/src/index.ts
@@ -1,49 +1,88 @@
 /**
- * 
- * @param target 被装饰的类
- * @returns 
+ * 成员装饰器d
+ * @param target 修饰实例属性的时候target 是类的 prototpye 修饰静态属性的时候 target 是 类的构造函数
+ * @param key  被装饰的属性属性名
  */
-function test(target:new(...args:any[]) => object) {
-   
+function d(target:any,key:string) {
+    console.log(target,key)
+
+    if(!target.__props) {
+        target.__props = [];
+    }
+    target.__props.push(key);
 }
-@test
+
+// 通过函数调用方式实现的成员装饰器 也称为装饰器工厂
+// function d () {
+//     return function(target:any,key:string) {
+//         console.log(target,key)
+//     }
+// }
+
 class A {
+    @d
+    // @d()
     prop1:string
 
-    constructor(public prop2:string) {}
+    @d
+    // @d()
+    static prop2:string
 }
 
-// const a = new A();
+console.log('A.prototype',(A.prototype as any).__props)
+
+const a = new A();
+console.log((a as any).__props);
+
+console.log('----------------------------------------------');
 
 
-function test2(str:string) {
-    return function (target:new (...args:any[]) => object) {
-        console.log('target',target)
-    } 
+/**
+ * 方法装饰器
+ * @param target  修饰实例方法的时候target 是类的 prototpye 修饰静态属性的时候 target 是 类的构造函数
+ * @param key  被装饰的方法名
+ * @param descriptor  修饰符对象
+ */
+// function enumerable() { 
+//     return function(target:any,key:string,descriptor:PropertyDescriptor) {
+//         console.log(target,key,descriptor);
+//         if(key === 'method1') {
+//             // 方法的修饰符对象默认是不可以遍历的，在修饰器中可以修改为参与遍历
+//             descriptor.enumerable = true
+//         }
+//     }
+// }
+
+function enumerable(target:any,key:string,descriptor:PropertyDescriptor) { 
+    console.log(target,key,descriptor);
+    if(key === 'method1') {
+        // 方法的修饰符对象默认是不可以遍历的，在修饰器中可以修改为参与遍历
+        descriptor.enumerable = true
+    }
 }
-@test2('这是一个类')
+function useless(target:any,key:string,descriptor:PropertyDescriptor) {
+    descriptor.value = function() {
+        console.warn(`${key}已过期`); 
+    }
+}
 class B {
-
-}
-
-type constructor = new (...args:any[]) => object
-
-function d1() {
-    console.log('d1');
-    return function(target:constructor) {
-        console.log('d1 decorator');
+    @enumerable
+    @useless
+    method1() {
+        console.log('method1')
     }
-}
 
-function d2() {
-    console.log('d2');
-    return function(target:constructor) {
-        console.log('d2 decorator');
+    @enumerable
+    method2() {
+
     }
-}
-
-@d1()
-@d2()
-class C {
 
 }
+
+const b = new B();
+
+for (const key in b) {
+    console.log('b',key)
+}
+
+b.method1()
