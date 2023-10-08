3087f1ac56382c985bc74267846930ff356ec295
t-jie.xu3 <18330156357@163.com>
2023-10-08 23:53:50 +0800

    class-validator class-transformer 完成

--- TypeScript进阶/03.装饰器/src/index.ts
+++ TypeScript进阶/03.装饰器/src/index.ts
@@ -1,54 +1,50 @@
-import {printObj, descriptor } from "./Descriptor";
+// import { IsNotEmpty, MaxLength, MinLength, validate } from 'class-validator';
 import 'reflect-metadata';
+// class RegUser {
+//     @IsNotEmpty({message:'账号不能为空'})
+//     @MinLength(5,{message:'账号至少五个字符'})
+//     @MaxLength(12,{message:'账号最多12 个字符'})
+//     loginId:string
+//     loginPwd:string
+//     @MinLength(0,{message:'年龄的最小值只能是 0'})
+//     @MaxLength(100,{message:'年龄的最大值只能是 100'})
+//     age:number
+//     gender:'男'|'女'
+// }
 
-@descriptor('用户')
+// const post = new RegUser();
+// post.loginId = ''
+// post.age = -1
+// validate(post).then(errors => {
+//     console.log('errors',errors)
+// })
+
+
+
+import axios from "axios";
+import { Type, plainToClass } from "class-transformer";
+
 class User {
-    @descriptor('账号')
-    loginId:string
-    @descriptor('密码')
-    loginPwd:string
+    id: number
+    firstName: string
+    lastName: string
+    @Type(() => Number)
+    age: number
+
+    getName() {
+        return this.firstName + ' ' + this.lastName;
+    }
+
+    isAdult() {
+        return this.age > 36 && this.age < 60;
+    }
 }
-
-const u = new User();
-u.loginId = 'abc'
-u.loginPwd = '123'
-printObj(u)
-
-// @descriptor('文章')
-// class Article {
-//     @descriptor('标题')
-//     title:string
-
-//     @descriptor('内容')
-//     content:string
-
-//     @descriptor('日期')
-//     date:Date
-// }
-
-// const ar = new Article();
-// ar.title = 'xxx';
-// ar.content = 'wjefklwejfkwjefwlfjwelfjwf';
-// ar.date = new Date();
-// printObj(ar)
-
-// @Reflect.metadata('a','一个类')
-// class A {
-//     @Reflect.metadata('prop','一个属性')
-//     prop1:string
-// }
-
-
-// const obj = new A();
-/**
- * 第一个重载入 取类的元数据信息
- * 第一个参数取元数据 
- */
-// console.log("Reflect.getMetadata('a',obj)",Reflect.getMetadata('a', A))
-
-/**
- * 第二个重载 取属性的元数据信息
- * 第一个参数 key 值 第二个参数对象 第三个参数 属性名称
- */
-
-// console.log(Reflect.getMetadata('prop',obj,'prop1'));
+axios.get('https://api.jsonserve.com/SQDyh4').then(res => res.data).then((users:User[]) => {
+    const us = plainToClass(User,users)
+    for (const u of us) {
+        // const user = plainToClass(User,u);
+        // console.log('user.getName()',user.getName(),user.isAdult())
+        console.log(typeof u.age,u.age);
+        
+    }
+})
