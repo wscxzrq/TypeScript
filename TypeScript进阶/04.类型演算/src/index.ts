c780a9d37fe2e64e49effdae1539bd37a45162e2
t-jie.xu3 <18330156357@163.com>
2023-10-09 23:17:58 +0800

    预定义类型演算

--- TypeScript进阶/04.类型演算/src/index.ts
+++ TypeScript进阶/04.类型演算/src/index.ts
@@ -1,87 +1,71 @@
-// typeof
-const a:string = 'asdff'
 
-let b:typeof a;
-
-class User {
-    loginId:string
-    loginPwd:string
-}
-
-// function createUser(cls:typeof User):User {
-//     return new cls();
-// }
-
-// const u = createUser(User)
-
-const u = new User();
-const u2 = u
-
-const A = User;
-new A();
-
-
-// keyof
+// Partial 可选
 interface User {
-    loginId:string
-    loginPwd:string
     age:number
+    name:string
 }
 
-interface Article {
-    title:string
-    publishDate:Date
+let u:Partial<User>;
+u = {}
+
+// Required 必填
+let a:Required<User>
+// a = {} // 必填报错
+
+// Readonly 只读
+let b:Readonly<User>
+// b.age = '' // 只读报错
+
+// Exclude 剔除
+{
+    type T = '男'|'女'|null|undefined;
+    type NEWT = Exclude<T,null|undefined>
 }
 
-function printUserPropery(obj:User,prop: keyof User) {
-    console.log(obj[prop]);
+// Extract 保留
+{
+    type T = '男'|'女'|null|undefined;
+    type NEWT = Extract<T,null|undefined>
 }
 
-const c:User = {
-    loginId: "",
-    loginPwd: "",
-    age: 0
+// NonNullable 剔除类型中的 null 和 undefined 也可是使用 str! 非空断言实现
+{
+    type str = string | null | undefined;
+    type strNotEmpty = NonNullable<str>
 }
 
-printUserPropery(c,'loginId')
+// ReturnType 获取函数返回值
+{
+    type func = () => number
+    type returnType = ReturnType<func>
 
-// in
-type UserString = {
-    [p in keyof User ]:User[p]
+    // 演算一个函数的返回值
+    function sum(a:string,b:string) {
+        return a + b
+    }
+    let a:ReturnType<typeof sum>
 }
 
-const d:UserString = {
-    loginId:'',
-    loginPwd:'',
-    age:1
-}
+// InstanceType 获取构造函数的返回类型
+{
+    class User {
 
-type UserReadonly = {
-   readonly [p in keyof User]:User[p]
-}
+    }
+    let u:InstanceType<typeof User>
 
-const e:UserReadonly = {
-    loginId: "",
-    loginPwd: "",
-    age: 0
-}
-// e.age = 1 // 只读报错
+    // 约束构造函数
+    class User2 {
+        loginId:string
+    }
+    type twoParamsConstructor = new (arg1:any,arg2:any) => User2
+    const A:twoParamsConstructor = class Test extends User2{
+        // 鸭子变形法 类的参数可以少不能多
+        // constructor(a:any,b:any,c:any){}
 
-
-// 可选的
-type UserPartial = {
-    [p in keyof User]?:User[p]
-}
-
-const f:UserPartial = {
-
-}
-
-// 搭配泛型使用
-type partial<T> = {
-    [p in keyof T]?:T[p]
-}
-
-const g:partial<Article> = {
-
+        constructor(a:any,b:any){
+            super()
+        }
+    }
+    
+    type Inst = InstanceType<twoParamsConstructor>
 }
