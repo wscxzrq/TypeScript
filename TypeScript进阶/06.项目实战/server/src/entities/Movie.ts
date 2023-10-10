67cff57f924544b3f81243f5af3b3546ae7b4f7c
t-jie.xu3 <18330156357@163.com>
2023-10-11 00:45:22 +0800

    平面对象转换 Movie 对象并细化校验规则

--- TypeScript进阶/06.项目实战/server/src/entities/Movie.ts
+++ TypeScript进阶/06.项目实战/server/src/entities/Movie.ts
@@ -1,32 +1,44 @@
-import { ArrayMinSize, IsInt, IsNotEmpty, Max, Min, MinLength } from 'class-validator'
+import { Type } from 'class-transformer';
+import { ArrayMinSize, IsArray, IsInt, IsNotEmpty, Max, Min, MinLength } from 'class-validator'
 export class Movie {
     @IsNotEmpty({message:'电影名称不可以为空'})
+    @Type(() => String) // 在运行阶段控制类型
     public name:string;
 
     @IsNotEmpty({message:'电影类型不可以为空'})
     @ArrayMinSize(1,{message:'电影类型至少存在一个'})
+    @IsArray({message:'电影类型必须是数组'})
+    @Type(() => String) // 如果需要字符串数组 则写中每一项的类型
     public types:string[];
 
     @IsNotEmpty({message:'上映地区不可以为空'})
     @ArrayMinSize(1,{message:'上映地区至少存在一个'})
+    @IsArray({message:'上映地区必须是数组'})
+    @Type(() => String)
     public areas:string[];
 
     @IsNotEmpty({message:'时长不可以为空'})
     @IsInt({message:'时长必须是整数'})
     @Min(1,{message:'时长最小一分钟'})
     @Max(999999,{message:'时长过长'})
+    @Type(() => Number)
     public timeLong:number;
 
     @IsNotEmpty({message:'是否热映不可以为空'})
+    @Type(() => Boolean)
     public isHot:boolean;
 
     @IsNotEmpty({message:'是否即将上映不可以为空'})
+    @Type(() => Boolean)
     public isComing:boolean;
 
     @IsNotEmpty({message:'是否是经典影片不可以为空'})
+    @Type(() => Boolean)
     public isClasic:boolean;
 
+    @Type(() => String)
     public description?:string; // 简介
 
-    public poster:string;
+    @Type(() => String)
+    public poster:string; // 海报
 }
--- TypeScript进阶/06.项目实战/server/src/index.ts
+++ TypeScript进阶/06.项目实战/server/src/index.ts
@@ -2,10 +2,17 @@
 // express
 // 验证：class-validator
 
+import 'reflect-metadata'
 import { validate } from 'class-validator'
 import { Movie } from "./entities/Movie";
-
-const m = new Movie();
+import { plainToClass } from 'class-transformer';
+const m:any = {};
+m.name = 123;
+m.types = [1,2,3];
+// 将 plain object 转换为一个 Movie 对象
+const movie = plainToClass(Movie,m as object);
+// movie.name = 23123
+console.log('movie',movie)
 validate(m).then(errors => {
     console.log('errors',errors)
 })
