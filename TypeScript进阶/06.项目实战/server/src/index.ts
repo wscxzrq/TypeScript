ba22cd7c73ca622d56e5d66493c2432a68e7690d
t-jie.xu3 <18330156357@163.com>
2023-10-17 23:40:51 +0800

    文件上传完成

--- TypeScript进阶/06.项目实战/server/src/index.ts
+++ TypeScript进阶/06.项目实战/server/src/index.ts
@@ -3,7 +3,12 @@ import * as Express from 'express';
 import MovieRouter from './routes/MovieRoute'
 import UploadRouter from './routes/UploadRouter'
 const app = Express();
+
+// 当请求 /upload 路径的时候，请求的是 public/upload 这个文件夹中的东西
+app.use('/upload',Express.static("public/upload"))
+
 app.use('/api/movie',MovieRouter)
+
 app.use(Express.json()); // 配置中间件，用于解析请求消息体中的 JSON 格式数据
 
 // 文件上传
--- TypeScript进阶/06.项目实战/server/src/routes/UploadRouter.ts
+++ TypeScript进阶/06.项目实战/server/src/routes/UploadRouter.ts
@@ -2,11 +2,52 @@
 import * as Express from "express";
 import * as multer from 'multer';
 import * as path from 'path'; // 处理路径的模块
+import { ResponseHelper } from "./ResponseHelper";
 const router = Express.Router();
-const upload = multer({dest:path.resolve(__dirname,'../../public/upload')});
 
-router.post('/',async (req,res) => {
+// 文件保存的配置
+const storage = multer.diskStorage({
+    // 文件存储位置
+    destination:path.resolve(__dirname,'../../public/upload'),
+    filename: function (req, file, cb) {
+        // 文件名是啥
+        const time = new Date().getTime();
+        // 后缀名是啥
+        const originalFileName = file.originalname;
+        const extname = path.extname(originalFileName) // 读取文件后缀名
+        // 设置文件的全称
+        cb(null, time + extname);
+    }
+})
 
+const allowedExtensions = ['.jpg','.png','.gif','.bmp','jiff']; // 白名单
+const upload = multer({ 
+    storage,
+    limits:{
+        fileSize:1024 * 1024 // 文件最大 1M
+    },
+    fileFilter:function(req,file,cb) {
+        const ext = path.extname(file.originalname);
+        if(allowedExtensions.includes(ext)) {
+            cb(null,true);
+        }else {
+            cb(new Error('文件类型不正确'));
+        }
+    }
+}).single('imgfile');
+
+
+router.post('/',(req,res) => {
+    upload(req,res, err => {
+        if(err) {
+            // 发生错误
+            ResponseHelper.sendError(err.message,res);
+        }else {
+            // 上传成功
+            const url = `/upload/${req.file?.filename}`
+            ResponseHelper.sendData(url,res);
+        }
+    })
 })
 
 export default router
