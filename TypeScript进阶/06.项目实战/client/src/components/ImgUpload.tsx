e61863e3dd934f9d076d93f794ac03d01cb27e14
t-jie.xu3 <18330156357@163.com>
2023-10-27 09:27:11 +0800

    图片上传 1

--- TypeScript进阶/06.项目实战/client/src/components/ImgUpload.tsx
+++ TypeScript进阶/06.项目实战/client/src/components/ImgUpload.tsx
@@ -2,14 +2,19 @@ import { Icon, Upload } from "antd";
 import React from "react";
 // 图片上传
 export default class extends React.Component {
-  render() {
-    return(
-      <Upload action='/api/upload' name="imgfile" accept=".jpg,.png,.gif" listType="picture-card">
-        <div>
+  private getUploadContent() {
+    if() {
+      <div>
           
           <Icon type="plus" />
           <div>点击上传</div>
       </div>
+    }
+  }
+  render() {
+    return(
+      <Upload action='/api/upload' name="imgfile" accept=".jpg,.png,.gif" listType="picture-card">
+        {this.getUploadContent()}
       </Upload>
     )
   }
