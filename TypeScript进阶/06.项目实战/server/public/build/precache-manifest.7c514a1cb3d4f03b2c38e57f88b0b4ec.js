545dc13fb4644bcf02d213e69f5f0c48c6d79cd0
t-jie.xu3 <18330156357@163.com>
2023-11-01 23:32:16 +0800

    项目打包

---
+++ TypeScript进阶/06.项目实战/server/public/build/precache-manifest.7c514a1cb3d4f03b2c38e57f88b0b4ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff74cf4b479cbcf650f2e16cb79c5ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba37963f55adb5df028e",
+    "url": "/static/css/2.4358233e.chunk.css"
+  },
+  {
+    "revision": "b35da220a430eb63314a",
+    "url": "/static/css/main.470099f9.chunk.css"
+  },
+  {
+    "revision": "ba37963f55adb5df028e",
+    "url": "/static/js/2.1b2e0df3.chunk.js"
+  },
+  {
+    "revision": "b35da220a430eb63314a",
+    "url": "/static/js/main.22347d35.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b4e877ba946a7e6a898ced905611c528",
+    "url": "/static/media/defaultposter.b4e877ba.png"
+  }
+]);
--- TypeScript进阶/06.项目实战/server/src/index.ts
+++ TypeScript进阶/06.项目实战/server/src/index.ts
@@ -2,10 +2,25 @@ import 'reflect-metadata'
 import * as Express from 'express';
 import MovieRouter from './routes/MovieRoute'
 import UploadRouter from './routes/UploadRouter'
+import * as  history from 'connect-history-api-fallback'
 const app = Express();
-
+app.use(history())
 // 当请求 /upload 路径的时候，请求的是 public/upload 这个文件夹中的东西
 app.use('/upload',Express.static("public/upload"))
+// 当请求 / 路径的时候，请求的是 public/upload 这个文件夹中的东西
+app.use('/',Express.static("public/build"))
+ 
+/**
+ * connect-history-api-fallback
+ * 由于 react 是单页应用，使用的是H5的 history API 所以当路由发生改变以后刷新页面会出现找不到文件的情况
+ * 使用 connect-history-api-fallback 库 来处理
+ * 无论访问的地址是什么，都会被映射为 http://localhost:3000/index.html
+ * 发生映射的条件
+ *  1. 请求是一个 GET 请求
+ *  2. 请求头里有一个 accept 必须是 text/html
+ *  3. 不是一个直接对文件的请求
+ *  4. 有没匹配到自定义规则
+ */
 
 app.use('/api/movie',MovieRouter)
 
